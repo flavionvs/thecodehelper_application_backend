42a365da5ca752848309d997cb82889234be0aae
Flavio Neves da Silva <flavionevesdasilva@thecodehelper.com>
2025-12-21 02:07:44 +1300

    Backend

--- app/Http/Controllers/API/ApiProjectController.php
+++ app/Http/Controllers/API/ApiProjectController.php
@@ -126,6 +126,7 @@ class ApiProjectController extends Controller
 
             $array = [];
             $array['id'] = $item->id;
+            $array['route_id'] = $item->my_row_id;
             $array['applied'] = Application::where('project_id', $item->id)->where('user_id', authId())->exists();
             $array['category_id'] = $item->category_id;
             $array['approved_freelancer_id'] = $item->approved_freelancer_id ?? null;
